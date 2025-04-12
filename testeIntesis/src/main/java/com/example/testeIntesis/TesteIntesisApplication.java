42ad8439d058fc2bae95137b71d5992340aebeeb
VHSNWLF <vitorhsnascimento.dev@gmail.com>
2025-04-11 23:18:29 -0300

    Initial configuration using SOLID and Clean Architecture, clienteEntity configuration in progress...

---
+++ testeIntesis/src/main/java/com/example/testeIntesis/TesteIntesisApplication.java
@@ -0,0 +1,13 @@
+package com.example.testeIntesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteIntesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteIntesisApplication.class, args);
+	}
+
+}
---
+++ testeIntesis/src/main/java/com/example/testeIntesis/domain/entities/clienteEntity.java
@@ -0,0 +1,9 @@
+package com.example.testeIntesis.domain.entities;
+
+public class clienteEntity {
+    private String nome;
+    private int id_produto;
+    private float total_venda;
+    private String cpf;
+    private int qtde_produto;
+}
